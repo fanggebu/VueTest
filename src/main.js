08463d3bf590337a0a6e2f012cdea01b02cf76cb
fanggebu <15128557367@163.com>
2021-02-01 20:01:48 +0800

    springboot + vue (vuex、element-ui)的登录实现

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+// import './icons'
+import router from './router' //引入路由配置
+import store from './store' //引入 Vuex 状态管理
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router, //使用路由
+  store,  //使用 Vuex 进行状态管理
+
 }).$mount('#app')
